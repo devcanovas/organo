8fbeb4c56ab3c93d3ea8e4d3e6237c1f88ec8dce
Matheus Canovas <matheuscanovas9@gmail.com>
2024-01-04 19:48:02 -0300

    Implementando funcionalidade de trocar cor primário das sessões de time

--- src/App.js
+++ src/App.js
@@ -2,17 +2,28 @@ import { useState } from 'react';
 import Banner from './components/Banner';
 import Form from './components/Form';
 import Team from './components/Team';
-import Mock from './components/utils/mocks';
+import Mock from './components/utils/Mock';
 
 function App() {
   const [workers, setWorkers] = useState(Mock.mockWorkers)
   const onNewWorkerRegistered = (worker) => {
     setWorkers([...workers, worker])
   }
-  const getTeamNames = Mock.mockTeams.map(team => team.name);
+  const [teams, setTeams] = useState(Mock.mockTeams);
 
-  const deleteWorker = () => {
+  const getTeamNames = teams.map(team => team.name);
+
+  function deleteWorker() {
     console.log("Deletando worker")
+  }
+
+  function changeTeamColor(color, nameTeam) {
+    setTeams(teams.map(team => {
+      if (team.name === nameTeam) {
+        team.primaryColor = color;
+      }
+      return team;
+    }))
   }
 
   return (
@@ -23,12 +34,13 @@ function App() {
         onRegisterWorker={(worker) => onNewWorkerRegistered(worker)}
       />
       {
-        Mock.mockTeams.map((team) => {
+        teams.map((team) => {
           return <Team
             key={team.name}
             name={team.name}
             primaryColor={team.primaryColor}
             secondaryColor={team.secondaryColor}
+            onChangeTeamColor={changeTeamColor}
             workers={workers.filter(worker => worker.time === team.name)}
             onDelete={deleteWorker}
           />
--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -1,15 +1,14 @@
 import WorkerCard from '../WorkerCard'
 import './Team.css'
 
-const Team = ({name, primaryColor, secondaryColor, workers, onDelete}) => {
+const Team = ({name, primaryColor, secondaryColor, workers, onDelete, onChangeTeamColor}) => {
     return (
         workers.length > 0 && <section className='team' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: secondaryColor }}>
-            <input value={secondaryColor} type='color' className='input-color'/>
+            <input value={primaryColor} onChange={event => {onChangeTeamColor(event.target.value, name)}} type='color' className='input-color'/>
             <h3 style={{ borderBottomColor: primaryColor }}>{name}</h3>
             <div className='workers'>
                 {
                     workers.map((worker) => {
-                        console.log('redering worker')
                         return <WorkerCard backgroundColor={primaryColor} key={worker.nome} worker={worker} onDelete={onDelete}/>
                     })
                 }
